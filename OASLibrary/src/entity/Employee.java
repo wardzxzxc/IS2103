fa7ecddeb3821748da68617ac7cbc70154e3cbf6
wardzxzxc <wardzxzxc@gmail.com>
2017-10-18 05:07:23 +0800

    Employee entity class added

---
+++ OASLibrary/src/entity/Employee.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import util.enumeration.EmployeeAccessRightsEnum;
+
+/**
+ *
+ * @author Edward
+ */
+@Entity
+public class Employee implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 32, unique = true)
+    private String username;
+    @Column(nullable = false, length = 32)
+    private String password;
+    @Column(nullable = false, length = 32)
+    private String firstName;
+    @Column(nullable = false, length = 32)
+    private String lastName;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private EmployeeAccessRightsEnum accessRight;
+    
+    
+    public Employee() {
+        
+        
+    }
+
+    public Employee(Long id, String username, String password, String firstName, String lastName, EmployeeAccessRightsEnum accessRight) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.accessRight = accessRight;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAccessRight(EmployeeAccessRightsEnum accessRight) {
+        this.accessRight = accessRight;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public EmployeeAccessRightsEnum getAccessRight() {
+        return accessRight;
+    }
+    
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Employee)) {
+            return false;
+        }
+        Employee other = (Employee) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Employee[ id=" + id + " ]";
+    }
+    
+}
